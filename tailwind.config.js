ef6fb51e087cd6c892df5fa8f83faeb3ed6ea191
omaty22 <omarcoursera@gmail.com>
2024-12-24 14:06:09 +0300

    fix bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,6 @@
 
 const plugin = require("tailwindcss/plugin");
 
-const myclass = plugin(function({ addUtilities}) {
-  addUtilities({
-    ".my-rotate-y-180": {
-      transform: "rotateY(180deg)",
-    },
-    ".preserve-3d": {
-      transformStyle: "preserve-3d",
-    },
-    ".prespective": {
-      perspective: "1000px",
-    },
-    ".backface-hidden": {
-      backfaceVisibility: "hidden",
-    },
-  });
-});
 
 export default {
   content: [
