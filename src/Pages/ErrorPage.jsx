a7af3d5d13be2cde1de7b9088a1c81fc0fe676a4
omaty22 <omarcoursera@gmail.com>
2024-12-26 12:16:05 +0300

    Errorpage beta

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -4,7 +4,7 @@ import animationData from '../assets/404.json'
 
 function ErrorPage() {
   return ( 
-    <div className='bg-black min-h-screen text-white flex flex-col items-center justify-center trnsition ease-in-out duration-700 gap-20'>
+    <div className='bg-slate-700 min-h-screen text-white flex flex-col items-center justify-center trnsition ease-in-out duration-700 gap-20'>
         <Lottie
             options={{
             loop: true,
@@ -17,7 +17,7 @@ function ErrorPage() {
             height={window.innerHeight -200}
             width={window.innerWidth -100}
         /> 
-        <a className='hover:text-slate-50 hover:bg-amber-700 text-3xl px-4 py-4 rounded-2xl' href="/Land">Go back</a>
+        <a className='hover:text-slate-50 hover:bg-black text-3xl px-4 py-4 rounded-2xl' href="/Land">Go back</a>
     </div>
   )
 }
