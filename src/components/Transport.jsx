dc1644a255005edf307cc34e5ac44ea95787117a
omaty22 <omarcoursera@gmail.com>
2025-01-03 16:25:48 +0300

    added most of the data for the transport page

--- src/components/Transport.jsx
+++ src/components/Transport.jsx
@@ -44,7 +44,7 @@ function Transport() {
         <DivLeft Hat='49 Sahil Evler' Mah='Barbarus mah, Süleymaniye mah, Anı Meydanı, Yeni diş hastanesi, Hilton AVM'/>
         <DivLeft Hat='Çarşı' Mah='Çarşı, Millet Parkı, Pac, Anı Myedanı'/>
         <DivLeft Hat='Primemall' Mah='Primall AVM merkezi,İSTE üniversitesi,Adliye'/>
-        <DivLeft Hat='Şekere' Mah=''/>
+        <DivLeft Hat='Şekere' Mah='Primemall,Adliye,Mustafa Kemal Mah.'/>
         <DivLeft Hat='Modern Evler' Mah=''/>
         {/*------------------- TAXİ-------------------*/}
         <DivLeft Hat='' Mah='İskendrun Taksi duraklarının konum ve numaralarını görüntülemek için:'/>
@@ -52,8 +52,10 @@ function Transport() {
     {/*------------------- iskenderun dışı-------------------*/}
     <div className='flex flex-col gap-4 mt-4'>
         <DivRight Hat='Antakya' Mah='Pac Araçları,zaman aralıkları için 0 326 225 35 38 arayabilirsiniz yada Sitelerini ziyaret etmek için :'link='https://www.antakyaotogari.com/koy-garajlari.php?firma=hatay-antakya-pac-kooperatifi'/>
-        <DivRight Hat='Belen' Mah='İskenderun - Antakya arası ulaşım için 0 326 225 35 38 arayabilirsiniz yada Sitelerini ziyaret etmek için :'link='https://www.antakyaotogari.com/koy-garajlari.php?firma=hatay-antakya-pac-kooperatifi'/>
-        
+        <DivRight Hat='Belen' Mah=  "Kışın Son Minibüs İSK'dan 23:30 de hareket eder Yazın son Minibüs İSK'dan 24:00 de hareket eder,ayrıntılı bilgi için :"link='http://belen.gov.tr/ulasim'/>
+        <DivRight Hat='Reyhanlı' Mah=  ",ayrıntılı bilgi için :"link=''/>
+        <DivRight Hat='Kırıkhan' Mah=  ",ayrıntılı bilgi için :"link=''/>
+        <DivRight Hat='Arsuz' Mah=  "Kalkış Saatleri:22.20, 22.50 ,23.40  günden güne saatlerde farklılık olabilir 0539 649 3063 aramanızı önermekteyiz. ayrıntılı bilgi için :"link='https://www.firmaulasim.com/iskenderun-arsuz-minibus-seferleri/'/>
     </div>
     </motion.div>
     </>
