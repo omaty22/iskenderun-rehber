2494583f70f6576613575bbf754283ad614f8f5b
omaty22 <omarcoursera@gmail.com>
2025-01-16 16:41:40 +0300

    added the SorularPage transition

--- src/components/Sorular.jsx
+++ src/components/Sorular.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import Navbar from '../components/Navbar';
 
 function Sorular() {
   return (
-    <div>
-      <p>hello</p>
-    </div>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="h-full bg-cover bg-center bg-slate-900"
+        id="Sorular"
+      >
+        <Navbar />
+    </motion.div> 
+    </>
   )
 }
 
