5e70d30a217946c5b140371c0043bc70c15b4a69
omaty22 <omarcoursera@gmail.com>
2025-01-17 14:20:22 +0300

    added the transition to the answers on the SorularPage

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,19 +1,31 @@
 import React ,{useState} from 'react'
+import { motion } from "framer-motion";
+import { Slide, Fade } from "react-awesome-reveal";
 
 function Accordion(props) {
     const [active, setActive] = useState(false);
   return (
-    <div className='py-3 mb-4 bg-gray-200 rounded-lg max-w-screen-2xl mx-2'>
+    <motion.div
+        initial={{ opacity: 0, x: -200 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+     className='py-3 mb-4 bg-gray-200 rounded-lg max-w-screen-2xl mx-2'>
       <button onClick={() => setActive(!active) } className='flex justify-between w-full'>
          <span className='pl-2 font-extrabold text-xl lg:text-3xl'>{props.title}</span>
          {active ? <span className='pr-3 lg:text-2xl'>-</span> : <span className='pr-3 lg:text-2xl'>+</span>}
-
       </button>
-      <div className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm
+      {active ?
+      <Fade cascade damping={0.05}>
+      <Slide>
+      <div className={`grid overflow-hidden transition-all duration-100 ease-in-out text-slate-600 text-sm
          ${active ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}>
         <div className='overflow-hidden pl-4 pt-2 lg:text-2xl'>{props.answer}</div>
       </div>
-    </div>
+      </Slide>  
+      </Fade>
+      : null}
+    </motion.div>
   )
 }
 
