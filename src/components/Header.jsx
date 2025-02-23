b919539fbe6f54d3bbbd8f82f490ab03331ae91b
omaty22 <omarcoursera@gmail.com>
2025-02-23 12:39:17 +0300

    top to bottom animasion on the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,18 +11,18 @@ function Header() {
       id="Header"
     >
       <motion.div
-        initial={{ opacity: 0, y: 100 }}
+        initial={{ opacity: 0, y: 0 }}
         transition={{ duration: 1.5 }}
-        whileInView={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: 1, y: 100 }}
         className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32"
       >
         <h2
           className="text-5xl sm:text-6xl md:text-[82] inline-block
-        max-w-3xl font-semibold pt-56 xl:text-8xl  xl:pt-96"
+        max-w-3xl font-semibold pb-40 xl:pb-0 xl:text-8xl xl:pt-96"
         >
           أهلا بكم في
         </h2>
-        <div className="space-x-6 mt-10">
+        <div className="space-x-6 pb-56 xl:mt-10">
           <a
             className="text-xl font-extrabold border border-black px-8 py-3 rounded hover:text-white hover:bg-black "
             href="#Footer"
